a4cb663bdd4f305c9d61f4a0aeaaed035cf1dd53
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2016-07-07 11:43:49 -0600

    Fixing routes

--- routes/login.js
+++ routes/login.js
@@ -3,3 +3,23 @@ var router = express.Router();
 var knex = require('../db/knex');
 var db = require('../db/api');
 var auth = require('../auth');
+
+router.get('/', function(req, res, next){
+  if(req.session.userId) {
+    res.redirect('/index');
+  } else {
+    next();
+  }
+}, function(req, res, next) {
+    res.render('./auth/login');
+});
+
+router.post('/', function(req, res, next){
+  auth.passport.authenticate('local', function(err, user, info){
+    if (err) {
+      res.render('./auth/', function(){
+
+      })
+    }
+  });
+});
