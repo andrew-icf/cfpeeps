774830bbfcc97889ed8219f9d47bcd9219f4e800
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2016-10-04 13:07:23 -0600

    Auth is working on localhost but not heroku

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local');
 
 passport.use(new LocalStrategy(function(username, password, done){
-  console.log("dummy->", username);
+  console.log(username);
   db.findUserByUsername(username).then(function(user, err) {
     if(!user) {
       done("Something went wrong, please try again");
